bc80bce74454079ad9393e09428c47b806dea4f4
trideb-religh <panigrahitrideb1@gmail.com>
2023-07-09 12:40:32 +0530

    updated

---
+++ frontend/src/Components/Register/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+        This is Register Page
+        <a href="/">To Login</a>
+    </div>
+  )
+}
+
+export default Register

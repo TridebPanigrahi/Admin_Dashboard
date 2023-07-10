21a109ab65cf31a82583888f79c9ca0f757800a7
trideb-religh <panigrahitrideb1@gmail.com>
2023-07-10 12:35:43 +0530

    updated

--- frontend/src/Components/Dashboard/Sections/BodySection/TopSection/Top.jsx
+++ frontend/src/Components/Dashboard/Sections/BodySection/TopSection/Top.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import './top.css'
-import './top.scss'
-import {BiSearchAlt} from 'react-icons/bi'
-import {TbMessageCircle} from 'react-icons/tb'
-import {IoNotificationsOutline} from 'react-icons/io5'
+import React from "react";
+import "./top.css";
+import "./top.scss";
+import { BiSearchAlt } from "react-icons/bi";
+import { TbMessageCircle } from "react-icons/tb";
+import { IoNotificationsOutline } from "react-icons/io5";
+import { BsArrowRightShort } from "react-icons/bs";
 
-import img from '../../../Assets/Trideb_panigrahi_IMG.jpg'
+import img from "../../../Assets/Trideb_panigrahi_IMG.jpg";
 
-import video from '../../../Assets/flowerflying.mp4'
+import img2 from "../../../Assets/dashboardImg3-removebg-preview.png";
 
-
+import video from "../../../Assets/flowerflying.mp4";
 
 const Top = () => {
   return (
-    <div className='topSection'>
+    <div className="topSection">
       <div className="headerSection flex">
         <div className="title">
           <h1>Welcome to Wecare.</h1>
@@ -21,12 +22,12 @@ const Top = () => {
         </div>
 
         <div className="searchBar flex">
-          <input type="text" placeholder='Search Dashboard' />
-          <BiSearchAlt className='icon'/>
+          <input type="text" placeholder="Search Dashboard" />
+          <BiSearchAlt className="icon" />
         </div>
         <div className="adminDiv flex">
-          <TbMessageCircle className='icon'/>
-          <IoNotificationsOutline className='icon'/>
+          <TbMessageCircle className="icon" />
+          <IoNotificationsOutline className="icon" />
           <div className="adminImage">
             <img src={img} alt="Admin Image" />
           </div>
@@ -35,7 +36,9 @@ const Top = () => {
       <div className="cardSection flex">
         <div className="rightCard flex">
           <h1>Create and sell extraordinary products</h1>
-          <p>The world's fast growing industry today are natural made products!</p>
+          <p>
+            The world's fast growing industry today are natural made products!
+          </p>
 
           <div className="buttons flex">
             <button className="btn">Explore More</button>
@@ -46,9 +49,51 @@ const Top = () => {
             <video src={video} autoPlay loop muted></video>
           </div>
         </div>
+
+        <div className="leftCard flex">
+          <div className="main flex">
+            <div className="textDiv">
+              <h1>My Stat</h1>
+
+              <div className="flex">
+                <span>
+                  Today
+                  <br />
+                  <small>4 Orders</small>
+                </span>
+                <span>
+                  This Month
+                  <br />
+                  <small>230 Orders</small>
+                </span>
+              </div>
+              <span className="flex link">
+                Go to my order <BsArrowRightShort className="icon" />
+              </span>
+            </div>
+
+            <div className="imgDiv">
+              <img src={img2} alt="Image Name" />
+            </div>
+            
+            {/* <div className="sideBarCard">
+              <BsQuestionCircle className="icon" />
+              <div className="cardContent">
+                <div className="circle1"></div>
+                <div className="circle2"></div>
+                <h3>Help Center</h3>
+                <p>
+                  Having trouble in Wecare, please contact us from for more
+                  questions.
+                </p>
+                <button className="btn">Go to help center</button>
+              </div>
+            </div> */}
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Top
+export default Top;
